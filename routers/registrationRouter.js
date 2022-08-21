a98d302ebd2205d3dbb7bd43a4d7b8360c72c186
unknown <kusokpocki@gmail.com>
2022-08-21 14:44:55 +0300

    verify token

--- routers/registrationRouter.js
+++ routers/registrationRouter.js
@@ -1,15 +1,30 @@
 import { Router } from "express";
+import { OAuth2Client } from "google-auth-library";
 import jwt from "jsonwebtoken";
 
 const regRouter = new Router()
+const clientId = "297750495683-im2hja8uvhhopfjp3rl0g0plkdplvoqf.apps.googleusercontent.com"
+
+
+const verifyToken = async (clId, token) => {
+    try {
+        const client = new OAuth2Client(clId)
+        const tiket = await client.verifyIdToken({
+            idToken: token,
+            audience: clId
+        })
+        const payload = tiket.getPayload()
+        return payload
+    } catch (e) {
+        console.log(e)
+    }
+}
 
 regRouter.post("/registration", async (req, res) => {
+    const verifyDecod = await verifyToken(clientId, req.body.token)
     try {
-        const bodyReq = req
-        const decodToken = jwt.decode(bodyReq.body.token)
-        console.log(decodToken)
         res.status(200).json({
-            decode: decodToken,
+            ...verifyDecod,
             serverMessage: "good reqvest"
         })
     } catch (e) {
